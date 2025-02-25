85673be9b26519cca5ab441fd87752efb41866f4
Vimalan Christopher <117967331+Vimalanchristopher@users.noreply.github.com>
2025-02-25 12:26:39 +0530

    Fixed deployment and CORS issues

--- server/server.js
+++ server/server.js
@@ -21,14 +21,27 @@ const allowedOrigins = [
     "http://localhost:3000"
 ];
 
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-
-app.options("*", cors());
 
 // Middleware
 app.use(express.json());
